d0cef45e31898dbb67250dfa5900d879c42dc133
hafsafathy <hafsafathy020@gmail.com>
2023-09-10 12:45:49 +0000

    8.task

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    l = len(sentence)
-
-    if (l == 0):
-        ntup = (l, None)
-    else:
-        ntup = (l, sentence[0])
-
-    return (ntup)
+    return (len(sentence), sentence[0] if len(sentence) > 0 else None)
