419b64c758c5a742844f9fc24663490a18bfb089
hafsafathy <hafsafathy020@gmail.com>
2023-09-26 22:04:22 +0000

    m

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,8 +4,8 @@
 class Square:
     """  class Square that defines a square by: (based on 0-square.py)"""
    def __init__(self, size):
-        """initialize square
-        Args:
-            size (int): size of the square
-        """
-        self.__size = size
+       """initialize square
+       Args:
+           size (int): size of the square
+       """
+        self.__size = size #: size of the square
