4e39999db44907414c4cd2cea870f623611b56a3
hafsafathy <hafsafathy020@gmail.com>
2023-10-10 22:41:30 +0000

    6.task

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""function that creates an Object from a JSON file
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """function
+
+    Args:
+        filename: textfile name
+
+    Raises:
+        Exception: when the object can't be encoded
+
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.load(f)
