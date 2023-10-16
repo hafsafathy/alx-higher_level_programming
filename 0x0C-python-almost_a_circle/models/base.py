9a4c4de130d191efc88402b5fe70c09d31581c9e
hafsafathy <hafsafathy020@gmail.com>
2023-10-16 08:58:43 -0700

    task

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""base model class"""
+class Base:
+    """
+    Class Base
+
+    Attributes:
+        nb_objects (int): Private class attribute
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Class constructor
+
+        Attributes:
+            id (int): An integer input for id
+        """
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns a json string representation
+
+        Attributes:
+            list_dictionaries (json): An inputted jason list of dictionaries
+        Return:
+            A json repreentation
+        """
+        if list_dictionaries is None or not list_dictionaries:
+            return '[]'
+        return json.dumps(list_dictionaries)
