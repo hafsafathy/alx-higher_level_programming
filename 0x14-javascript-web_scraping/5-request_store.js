58913190c8b1715e7353bcf4046859c1f48186a1
hafsafathy <hafsafathy020@gmail.com>
2024-01-30 05:25:09 +0000

    m

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,8 @@
+const req = require('request');
+const fs = require('fs');
+
+req(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    fs.writeFileSync(process.argv[3], body);
+  }
+});
